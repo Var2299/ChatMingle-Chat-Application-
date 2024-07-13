da33bf377fdc1225dad3cb9a4f20ca04cec5b041
Var2299 <125533314+Var2299@users.noreply.github.com>
2024-07-14 00:12:50 +0530

    Add files via upload

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css';
+
+const Header = () => {
+  return (
+    <div className='head'>
+    
+    <h1>ChatMingle</h1>
+  </div>  
+  )
+}
+
+export default Header
